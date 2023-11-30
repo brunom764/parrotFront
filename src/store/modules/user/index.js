8220249dd57cfd1e8a5a92d99104bcfab4f90e49
guilopesrbc <glrbc@cin.ufpe.br>
2023-11-30 19:54:18 -0300

    Feature: cadastrar

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -20,10 +20,17 @@ export const mutations = {
 
 export const actions = {
   async createUser({ commit }, {email, password}) {
-    const response = await axios.post(`${process.env.VUE_APP_SERVER_URL}/identity/create`, {email, password});
-    commit('updateField', { path: 'idUser', value: response.data.idUser })
-    return response.data;
+    commit('updateField', { path: 'loading', value: true })
+    try {
+      const result = await axios.post(`${process.env.VUE_APP_SERVER_URL}/identity/create`, {email, password})
+      commit('updateField', { path: 'loading', value: false })
+      return result
+    } catch (error) {
+      commit('updateField', { path: 'loading', value: false })
+      return error
+    }
   },
+
   async loginUser({ commit }, {email, password}) {
     const response = await axios.post(`${process.env.VUE_APP_SERVER_URL}/login`, {email, password});
     commit('updateField', { path: 'idUser', value: response.data.idUser })
