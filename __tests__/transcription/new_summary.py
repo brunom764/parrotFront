c3fc0bc0e55463398acce9913902a4b92a27ebe9
guilopesrbc <glrbc@cin.ufpe.br>
2024-03-19 15:08:33 -0300

    ajuste em elementos de testes

--- __tests__/transcription/new_summary.py
+++ __tests__/transcription/new_summary.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome() 
-driver.get('https://parrot-ruddy.vercel.app/')
+driver.get('http://localhost:8080/login')
 
 #login
 email_input = driver.find_element("name", "email")
@@ -14,19 +14,24 @@ time.sleep(2)
 #envio de dados de login
 email_input.send_keys("teste@gmail.com")
 password_input.send_keys("123456")
+login_button.click()
 
 time.sleep(2)
 
-login_button.click()
+expected_url = 'http://localhost:8080/dashboard' # Checa se a url esperada é a url de dashboard, ou seja, foi redirecionado para a página de dashboard
+current_url = driver.current_url
+
+nome_resumo_button = driver.find_element("xpath",'//span[text()="Nova transcrição"]')  
+nome_resumo_button.click()
 
 #criar novo resumo
-nome_resumo_input = driver.find_element("xpath",'/html/body/div[2]/div/div[2]/div/div[3]/div/div/div[1]/div/div[1]/div')  
+nome_resumo_input = driver.find_element("xpath",'//input[@label="Insira um título para a transcrição"]')  
 nome_resumo_input.send_keys("Entrevista do teste")
 
 time.sleep(2)
 
 #upar arquivo de audio
-audio_input = driver.find_element("xpath", '/html/body/div[2]/div/div[2]/div/div[3]/div/div/div[2]/input')
+audio_input = driver.find_element("xpath", '//input[@type="file" and @accept="audio/*"]')
 audio_input.click()
 audio_file_path = "/caminho/para/o/arquivo/audio.mp4"  # Substitua pelo caminho do arquivo de áudio no seu sistema
 audio_input.send_keys(audio_file_path)
@@ -34,7 +39,7 @@ audio_input.send_keys(audio_file_path)
 time.sleep(2)
 
 #enviar arquivo de audio
-enviar_button = driver.find_element("xpath",'/html/body/div[2]/div/div[2]/div/div[3]/div/div/div[3]/button')  
+enviar_button = driver.find_element("xpath",'//span[text()="Enviar"]')  
 enviar_button.click()
 
 time.sleep(5)
